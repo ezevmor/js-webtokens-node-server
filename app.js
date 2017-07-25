b8746d63d0a892d1fbee86c7db23b106eacfe877
Elio Zevallos More <e.zevallos-ext@dominion.dominion.es>
2017-07-25 09:41:01 +0200

    habilitado cors-domain

--- app.js
+++ app.js
@@ -8,18 +8,33 @@ app.use(bodyParser.json()); //habilita la recepcion de datos desde body en forma
 app.use(bodyParser.urlencoded({ extended: false })); //habilita la recepcion de datos desde body en formato application/x-www-form-urlencoded
 
 var jsonData = {
-    uno:'uno',
-    dos:'dos'
-};
+    options:[
+        {
+            name:"opcion1"
+        },
+        {
+            name:"opcion2"
+        },
+        {
+            name:"opcion3"
+        }
+    ]
+}
 
 var userAuth = {
     user: 'usuario',
     password: 'mipassword'
 };
 
+//habilita cors origin
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, token, Accept");
+    next();
+})
+
 app.get('/api/securedata',function(req,res){
     if(req.headers.token){
-
         jwt.verify(req.headers.token,authConfig.secretKey,function(err,decoded){
             if(err){
                 res.end('token no valido');
